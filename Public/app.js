dda0f97ef52e55defd5da314ac05776911a21da5
ThunyatepZ <taptitan4@gmail.com>
2022-02-07 22:02:25 +0700

    Change name to grade cal

--- Public/app.js
+++ Public/app.js
@@ -1,10 +1,27 @@
-let user = 101;
+function Show() 
+{
+    var score = prompt("กรอกคำแนนของคุณ")
 
-
-alert("Welcome to BMI CAL" + user)
-alert(user)
-console.log("Web start")
-
-var Debug_start = "start"
-
-console.log(Debug_start)
+    if (score >= 100) {
+        document.getElementById("screen").innerHTML = "ยินดีด้วยคุณได้คะแนนเต็ม"
+    }
+    else if(score >= 90){
+        document.getElementById("screen").innerHTML = "คุณได้เกรดA"
+    }
+    else if(score >= 80){
+        document.getElementById("screen").innerHTML = "คุณได้เกรดB"
+    }
+    else if(score >= 70){
+        document.getElementById("screen").innerHTML = "คุณได้เกรดC"
+    }
+    else if(score >= 60){
+        document.getElementById("screen").innerHTML = "คุณได้เกรดD"
+    }
+    else if(score >= 50){
+        document.getElementById("screen").innerHTML = "คุณได้เกรดE"
+    
+    }
+    else{
+        document.getElementById("screen").innerHTML = "เสียใจด้วยคุณสอบตก"
+    }
+}
